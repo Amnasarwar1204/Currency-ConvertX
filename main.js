b50aeaf777feded7948d4181e907be6540080bc0
Amnasarwar1204 <157794672+Amnasarwar1204@users.noreply.github.com>
2024-04-16 14:57:45 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,55 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+console.log(chalk.yellowBright("<<<<<<<<<WELCOME TO THE CURRENCY EXCHANGE>>>>>>>>>"));
+async function convertCurrency() {
+    let userAns = await inquirer.prompt([
+        {
+            name: 'from',
+            message: 'Enter from currency code',
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: 'to',
+            message: 'Enter to currency code',
+            type: "list",
+            choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+        },
+        {
+            name: 'amount',
+            message: 'Enter your Amount',
+            type: "number"
+        }
+    ]);
+    console.log("User input:", userAns);
+    let fromRate = currency[userAns.from];
+    let toRate = currency[userAns.to];
+    let amount = userAns.amount;
+    console.log("From Rate", userAns.from);
+    console.log("To Rate", userAns.to);
+    console.log("Amount", amount);
+    let convertAmount = (amount / fromRate) * toRate;
+    console.log("Converted Amount:", convertAmount);
+    let select = await inquirer.prompt([
+        {
+            name: "continue",
+            type: "confirm",
+            message: "Do you want to continue?",
+        }
+    ]);
+    if (select.continue) {
+        convertCurrency();
+    }
+    else {
+        console.log(chalk.greenBright("Goodbye"));
+    }
+    ;
+}
+convertCurrency();
